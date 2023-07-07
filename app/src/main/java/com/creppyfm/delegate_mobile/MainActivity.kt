a5a4577b1ce4a46999977151bb5b6975a1efee54
creppyfm <97473588+creppyfm@users.noreply.github.com>
2023-07-07 14:53:38 -0400

    Removing sensitive info.

--- app/src/main/java/com/creppyfm/delegate_mobile/MainActivity.kt
+++ app/src/main/java/com/creppyfm/delegate_mobile/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHost
 import com.creppyfm.delegate_mobile.ui.theme.DeleGate_MobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +23,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
+                    }
 
                 }
             }
